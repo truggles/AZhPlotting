04eba9b28d2d86a333dd3bd2b8c68301fdf28ba7
Tyler Ruggles <truggles@wisc.edu>
2015-05-01 16:28:09 -0500

    Fit is working, lots to fix

--- deco/intensityDist.py
+++ deco/intensityDist.py
@@ -1,35 +1,39 @@
 from ROOT import gROOT, TCanvas, TF1, TMath
+import ROOT
 import time
 import math
  
 gROOT.Reset()
 c1 = TCanvas( 'c1', 'Example with Formula', 200, 10, 700, 500 )
  
-#
-# Create a one dimensional function and draw it
-#
-
-#def myFunc(x):
-#    y = math.sin(x)
-#    return y
-
-#c = 1.5
-#fun1 = TF1( 'fun1', 'abs(sin(x)/x) + [0]*x', 0, 10 )
-#funX = TF1( 'funX', 'cos( ( x / [0]) )*cos( ( x / [0]) )', 0, 10 )
 fun5 = TF1( 'fun5', 'cos( TMath::ATan( x / [0]) )*cos( TMath::ATan( x / [0]) )', 0, 100 )
 fun10 = TF1( 'fun10', 'cos( TMath::ATan( x / [0]) )*cos( TMath::ATan( x / [0]) )', 0, 100 )
 fun50 = TF1( 'fun50', 'cos( TMath::ATan( x / [0]) )*cos( TMath::ATan( x / [0]) )', 0, 100 )
-#funZ = TF1( 'funZ', 'TMath::ATan(x)', 0, 10 )
-#fun2 = TF1( 'fun2', 'abs(cos(x)/x)', 0, 10 )
-#fun1 = TF1( 'fun1', 'myFunc(x)', 0, 10 )
+
 fun5.SetParameter(0, 5)
 fun10.SetParameter(0, 10)
 fun50.SetParameter(0, 50)
+
 c1.SetGridx()
 c1.SetGridy()
 fun5.Draw()
 fun10.Draw('same')
 fun50.Draw('same')
-#fun2.Draw('same')
 c1.Update()
+c1.SaveAs("initial_draw.png")
+c1.Close()
+
+c2 = TCanvas( 'c2', 'Example with Formula', 200, 10, 700, 500 )
+c2.SetGridx()
+c2.SetGridy()
+
+hist = ROOT.TH1F('h1','test', 100, 0 ,100)
+hist.FillRandom('fun5',20000)
+hist.Draw()
+f1 = gROOT.GetFunction('fun5')
+f1.SetParameter( 0, 800 )
+hist.Fit('fun5')
+c2.Update()
+
+
 time.sleep(3)
