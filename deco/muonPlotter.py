709097d6ed6d3b7aa4eb2b584240d97acec85c62
Tyler Ruggles <truggles@wisc.edu>
2015-05-07 20:58:40 -0500

    2d hist working, incorporated smaller area SPH camara

--- deco/muonPlotter.py
+++ deco/muonPlotter.py
@@ -1,5 +1,5 @@
 import os
-from ROOT import gROOT
+from ROOT import gROOT, gStyle
 import ROOT
 from array import array
 
@@ -7,7 +7,7 @@ nBins = 50
 nMax = 100
 
 mapper = { 'HTC_A510' : ('1', '2'),
-           'SPH-D710VMUB' : ('1'),
+           'SPH-D710VMUB' : ('1', 'small'),
            'RAZR' : ('1', '2', '3', '4'),
            #'SAMSUNG-SGH' : ('1')
 }
@@ -28,12 +28,15 @@ for key in mapper.keys():
   if 'HTC' in key:
     nBins = 10
     nMax = 100
+    lenMax = 30
   if 'SPH' in key:
     nBins = 10
     nMax = 70
+    lenMax = 30
   if 'RAZR' in key:
     nBins = 10
     nMax = 40 
+    lenMax = 20
 
   ofile = open('muons_%s.txt' % key, 'w')
   ofile.write('Image : Eccentricites : Len1 : Len 2\n')
@@ -41,6 +44,8 @@ for key in mapper.keys():
   lHist90 = ROOT.TH1F('%slength2' % key, '%s, Length of Muon Tracks, ecc > 0.9' % key, nBins, 0, nMax)
   lHist95 = ROOT.TH1F('%slength3' % key, '%s, Length of Muon Tracks, ecc > 0.95' % key, nBins, 0, nMax)
   lHist99 = ROOT.TH1F('%slength4' % key, '%s, Length of Muon Tracks, ecc > 0.99' % key, nBins, 0, nMax)
+  lenVsEcc = ROOT.TH2I('lenVsEcc', 'Length vs. Eccentricity', 100, 0.0, lenMax, 100, 0, 1)
+  lenVsEcc2 = ROOT.TH2I('lenVsEcc2', 'Length vs. Eccentricity2', 100, 0.0, lenMax, 100, 0.5, 1)
   #lHistAll = ROOT.TH1F('%slength1' % key, '%s, Length of Muon Tracks, ecc = All' % key, nBins, nMax)
   #lHist90 = ROOT.TH1F('%slength2' % key, '%s, Length of Muon Tracks, ecc > 0.9' % key, nBins, nMax)
   #lHist95 = ROOT.TH1F('%slength3' % key, '%s, Length of Muon Tracks, ecc > 0.95' % key, nBins, nMax)
@@ -49,7 +54,7 @@ for key in mapper.keys():
   #print len(mapper[key])
   for i in mapper[key]:
     #print mapper[key]
-    #print i
+    print i
     name = '%s%s' % (key, i)
     #print name
     ifile = open('%s_log.out' % name, 'r')
@@ -69,21 +74,30 @@ for key in mapper.keys():
         ofile.write('%10s %10f %10f %10f' % (info[0], ecc, l1, l2) )
         #if l1 > l2: len = l1
         #else: len = l2
-        len = l2
+        len_ = l2
+
+        # Give me some info on the low length high ecc events
+#        if len_ < 10 and ecc > 0.99:
+#            print "Evt: %s, Len: %f, Ecc: %f" % (str(info[0]), len_, ecc)
+        # Always fill the length vs eccentricity plot
+        lenVsEcc.Fill( len_, ecc)
+        lenVsEcc2.Fill( len_, ecc)
  
         # Does the candidate pass the lower eccentricity / vertical candadate cut?
         passing = False
+#        if 'SPH' in key and mapper[key][1] == 'small':
+#            print "%f, %f" % (len_, ecc)
         if ecc > 0.9:
             if area < 30:
                 if l1 / l2 < 3: passing = True
 
-        lHistAll.Fill( len )
+        lHistAll.Fill( len_ )
         if float( ecc ) > 0.99 or passing:
-            lHist99.Fill( len )
+            lHist99.Fill( len_ )
         if float( ecc ) > 0.95 or passing:
-            lHist95.Fill( len )
+            lHist95.Fill( len_ )
         if float( ecc ) > 0.90:
-            lHist90.Fill( len )
+            lHist90.Fill( len_ )
 
   # Fitting now
 #  def myfunc( x ):
@@ -117,10 +131,16 @@ for key in mapper.keys():
   pad4.Close()
   lHist99.Draw('hist e1')  
 
-  c1.SaveAs('muonsPlot_%s.png' % name)
+  c1.SaveAs('muonsPlot_%s.png' % key)
   c1.Close()
   c2 = ROOT.TCanvas("c1","title",600,600)
   lHist99.Draw('hist e1')
+  if not 'SPH' in key:
+    lHist99.SetBinContent(1, 0)
+    lHist99.SetBinError(1, 0)
+#  lHist99.SetBinContent(2, 0)
+#  lHist99.SetBinError(2, 0)
+#  lHist99.SetMaximum( lHist99.GetMaximum() * 10)
 
   funx = ROOT.TF1( 'funx', '[0]*cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', 0, 100 )
   f1 = gROOT.GetFunction('funx')
@@ -132,8 +152,19 @@ for key in mapper.keys():
   fitResult = lHist99.GetFunction("funx")
   fitResult.Draw('same')
   c2.Update()
+  c2.SaveAs('finalFit%s.png' % key)
+  c2.Close()
 
-  c2.SaveAs('finalFit%s.png' % name)
+  c3 = ROOT.TCanvas("c3","title",1000,500)
+  c3.Divide(2,1)
+  c3.cd(1)
+  lenVsEcc.Draw('COLZ')
+  c3.cd(2)
+  lenVsEcc2.Draw('COLZ')
+  gStyle.SetOptStat( 0 )
+  c3.SaveAs('lenVsEcc_%s.png' % key)
+  c3.Close()
+
   gROOT.cd()
 
 
